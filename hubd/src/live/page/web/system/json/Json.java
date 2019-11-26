236a29276c144b5beefd43909cd5d3980f6c6a98
pagetronic <pagetronic@gmail.com>
2019-11-26 19:34:39 +0400

    comment and clean

--- hubd/src/live/page/web/system/json/Json.java
+++ hubd/src/live/page/web/system/json/Json.java
@@ -21,16 +21,24 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+/**
+ * A cool toolkit for Json manipulation
+ */
 public class Json implements Map<String, Object>, Serializable, Bson {
 
-	private static final long serialVersionUID = 10009990999099099L;
-
+	//The real store
 	private final LinkedHashMap<String, Object> datas;
 
+	/**
+	 * Init empty
+	 */
 	public Json() {
 		datas = new LinkedHashMap<>();
 	}
 
+	/**
+	 * Init this a Json string, parse and make
+	 */
 	public Json(String json_string) {
 
 		datas = new LinkedHashMap<>();
@@ -51,6 +59,22 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Init and put Map object entries
+	 *
+	 * @param map for initialisation
+	 */
+	public Json(Map<String, Object> map) {
+
+		datas = new LinkedHashMap<>();
+		datas.putAll(map);
+	}
+
+	/**
+	 * Init and put anything have entries
+	 *
+	 * @param obj for initialisation
+	 */
 	public Json(Object obj) {
 		datas = new LinkedHashMap<>();
 		if (obj == null) {
@@ -59,17 +83,18 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		putAll((Map<String, Object>) obj);
 	}
 
+	/**
+	 * Init with one entry
+	 *
+	 * @param key   to put where
+	 * @param value to put at key
+	 */
 	public Json(String key, Object value) {
 
 		datas = new LinkedHashMap<>();
 		datas.put(key, value);
 	}
 
-	public Json(Map<String, Object> map) {
-
-		datas = new LinkedHashMap<>();
-		datas.putAll(map);
-	}
 
 	private <T> T get(String key, Class<T> clazz) {
 		try {
@@ -86,19 +111,11 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
-
-	public Object get(String key, Object def) {
-
-		if (get(key) == null) {
-			return def;
-		}
-		return get(key);
-	}
-
-	public Object get(String key) {
-		return datas.get(key);
-	}
-
+	/**
+	 * Get the ID as _id or id
+	 *
+	 * @return the id as String
+	 */
 	public String getId() {
 		if (containsKey("_id")) {
 			return getString("_id");
@@ -108,7 +125,35 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 	}
 
 	/**
-	 * @param key
+	 * Get anything from key
+	 *
+	 * @param key where find value
+	 * @return value at key
+	 */
+	public Object get(String key) {
+		return datas.get(key);
+	}
+
+	/**
+	 * Get anything from key with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return value at key
+	 */
+	public Object get(String key, Object def) {
+
+		if (get(key) == null) {
+			return def;
+		}
+		return get(key);
+	}
+
+
+	/**
+	 * Get a string with a length limitation
+	 *
+	 * @param key where find the value
 	 * @return null or a truncated string
 	 */
 	public String getString(String key) {
@@ -122,6 +167,13 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return Fx.truncate(val, 1000);
 	}
 
+	/**
+	 * Get a string with a length limitation with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return null or a truncated string
+	 */
 	public String getString(String key, String def) {
 		if (containsKey(key) && get(key) != null) {
 			return getString(key);
@@ -129,6 +181,13 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return def;
 	}
 
+	/**
+	 * Get a string with a limitation to possibles values
+	 *
+	 * @param key       where find the value
+	 * @param possibles values
+	 * @return null or a value in possibles parameter
+	 */
 	public String getChoice(String key, String... possibles) {
 		for (String possible : possibles) {
 			if (possible.equals(get(key))) {
@@ -138,10 +197,25 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return null;
 	}
 
+
+	/**
+	 * Get a string
+	 *
+	 * @param key where find the value
+	 * @return null or a string
+	 */
 	public String getText(String key) {
 		return get(key, String.class);
 	}
 
+
+	/**
+	 * Get a string with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return null or a string
+	 */
 	public String getText(String key, String def) {
 		if (containsKey(key)) {
 			String text = getText(key);
@@ -153,6 +227,14 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return def;
 	}
 
+
+	/**
+	 * Get a boolean with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return true or false
+	 */
 	public boolean getBoolean(String key, boolean def) {
 		try {
 			if (containsKey(key) && get(key) != null) {
@@ -170,10 +252,24 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a Date object at key
+	 *
+	 * @param key where find the value
+	 * @return null or a Date object
+	 */
 	public Date getDate(String key) {
 		return get(key, Date.class);
 	}
 
+
+	/**
+	 * Get a Date object at key with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return null or a Date object
+	 */
 	public Date getDate(String key, Date def) {
 		if (get(key) == null) {
 			return def;
@@ -181,6 +277,13 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return get(key, Date.class);
 	}
 
+	/**
+	 * Parse a string value as a Date object
+	 * Needed to parse when Json is created from a string
+	 *
+	 * @param key where find the value
+	 * @return null or a Date object
+	 */
 	public Date parseDate(String key) {
 		if (getString(key) == null) {
 			return null;
@@ -194,6 +297,12 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a Integer at key
+	 *
+	 * @param key where find the value
+	 * @return int value
+	 */
 	public int getInteger(String key) {
 		if (get(key).getClass().equals(Long.class)) {
 			return get(key, Long.class).intValue();
@@ -205,6 +314,13 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a Integer at key with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return int value
+	 */
 	public int getInteger(String key, int def) {
 
 		try {
@@ -221,6 +337,12 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a Double at key
+	 *
+	 * @param key where find the value
+	 * @return double value
+	 */
 	public double getDouble(String key) {
 
 		try {
@@ -230,6 +352,13 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a Double at key with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return double value
+	 */
 	public double getDouble(String key, double def) {
 		if (datas.get(key) == null) {
 			return def;
@@ -245,6 +374,14 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+
+	/**
+	 * Get a BigDecimal at key with a failure value
+	 *
+	 * @param key where find the value
+	 * @param def returned if there is no entry at key
+	 * @return BigDecimal value
+	 */
 	public BigDecimal getBigDecimal(String key, double def) {
 
 		if (get(key) instanceof Decimal128) {
@@ -268,14 +405,57 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return BigDecimal.valueOf(def);
 	}
 
+	/**
+	 * Get a List of string
+	 *
+	 * @param key where find the values
+	 * @return List of string values
+	 */
 	public List<String> getList(String key) {
 		return getList(key, String.class);
 	}
 
+	/**
+	 * Get a List of object in a specific class
+	 *
+	 * @param key   where find the values
+	 * @param clazz Class of the objects in the result
+	 * @return List of objects values
+	 */
 	public <T> List<T> getList(String key, Class<T> clazz) {
 		return (List<T>) datas.get(key);
 	}
 
+	/**
+	 * Get a Json
+	 *
+	 * @param key where find the json
+	 * @return Json value
+	 */
+	public Json getJson(String key) {
+		if (get(key) == null) {
+			return null;
+		}
+		try {
+			if (get(key).getClass().equals(Json.class)) {
+				return get(key, Json.class);
+			} else if (get(key).getClass().equals(Document.class)) {
+				return new Json(get(key, Document.class));
+			} else {
+				return new Json(get(key));
+			}
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	/**
+	 * Get a List of Jsons
+	 *
+	 * @param key where find the jsons
+	 * @return List of Jsons values
+	 */
 	public List<Json> getListJson(String key) {
 
 		if (key == null || get(key) == null) {
@@ -296,59 +476,91 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return outlist;
 	}
 
-	public Json getJson(String key) {
-		if (get(key) == null) {
-			return null;
-		}
-		try {
-			if (get(key).getClass().equals(Json.class)) {
-				return get(key, Json.class);
-			} else if (get(key).getClass().equals(Document.class)) {
-				return new Json(get(key, Document.class));
-			} else {
-				return new Json(get(key));
-			}
-		} catch (Exception e) {
-			return null;
-		}
 
-	}
-
+	/**
+	 * Get Binary Data at a key
+	 *
+	 * @param key where find the binary
+	 * @return binary value
+	 */
 	public Binary getBinary(String key) {
 		return get(key, Binary.class);
 	}
 
+	/**
+	 * Count entries in this object
+	 *
+	 * @return number of entries
+	 */
 	@Override
 	public int size() {
 		return datas.size();
 	}
 
+	/**
+	 * Is this object empty ?
+	 *
+	 * @return true if empty
+	 */
 	@Override
 	public boolean isEmpty() {
 		return datas.isEmpty();
 	}
 
+	/**
+	 * Is this object contains entry at this key ?
+	 *
+	 * @param key where to find entry
+	 * @return true if contains
+	 */
 	@Override
 	public boolean containsKey(Object key) {
 		return datas.containsKey(key);
 	}
 
+	/**
+	 * Is this object contains this value ?
+	 *
+	 * @param value to find in this object
+	 * @return true if contains
+	 */
 	@Override
-	public boolean containsValue(Object val) {
-		return datas.containsValue(val);
+	public boolean containsValue(Object value) {
+		return datas.containsValue(value);
 	}
 
+	/**
+	 * Get entry at a specific key
+	 *
+	 * @param key where find the value
+	 * @return the entry value
+	 */
 	@Override
 	public Object get(Object key) {
 		return datas.get(key);
 	}
 
+	/**
+	 * Put a value in this Json object at the end
+	 *
+	 * @param key   where to put an entry
+	 * @param value to put in entry
+	 * @return this object
+	 */
 	public Json put(String key, Object value) {
 		datas.remove(key);
 		datas.put(key, value);
 		return this;
 	}
 
+
+	/**
+	 * Put a value in this Json object at first
+	 *
+	 * @param key   where to put an entry
+	 * @param value to put in entry
+	 * @return this object
+	 */
 	public Json prepend(String key, Object value) {
 		datas.remove(key);
 		Json clone = new Json(this);
@@ -358,35 +570,78 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return this;
 	}
 
+	/**
+	 * Put a value in this Json object at the same place if exists, or at the end
+	 *
+	 * @param key   where to put an entry
+	 * @param value to put in entry
+	 * @return this object
+	 */
 	public Json set(String key, Object value) {
 		datas.put(key, value);
 		return this;
 	}
 
-	@Override
-	public Object remove(Object key) {
-		return datas.remove(key);
-	}
 
+	/**
+	 * Put all entry of a Map object
+	 *
+	 * @param map where entries have to be putted
+	 */
 	@Override
 	public void putAll(Map<? extends String, ?> map) {
 		datas.putAll(map);
 	}
 
+	/**
+	 * Put all entry of a Json object
+	 *
+	 * @param map where entries have to be putted
+	 */
 	public Json putAll(Json map) {
 		datas.putAll(map);
 		return this;
 	}
 
+	/**
+	 * Json contain entry at this key ?
+	 *
+	 * @param key where to find entry
+	 * @return true if Json contain entry
+	 */
 	public boolean containsKey(String key) {
 		return datas.containsKey(key);
 	}
 
+	/**
+	 * Remove entry
+	 *
+	 * @param key where to find the entry
+	 * @return this Json object
+	 */
 	public Json remove(String key) {
 		datas.remove(key);
 		return this;
 	}
 
+	/**
+	 * Remove value
+	 *
+	 * @param object to remove
+	 * @return value of this entry
+	 */
+	@Override
+	public Object remove(Object object) {
+		return datas.remove(object);
+	}
+
+	/**
+	 * Create or add value to a List in this object
+	 *
+	 * @param key   where to find or create a List
+	 * @param value to add to the List
+	 * @return this Json object
+	 */
 	public Json add(String key, Object value) {
 		List<Object> values = getList(key, Object.class);
 		if (values == null) {
@@ -396,6 +651,14 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return put(key, values);
 	}
 
+	/**
+	 * Create or add value to a List in this object at a specific position
+	 *
+	 * @param key      where to find or create a List
+	 * @param value    to add to the List
+	 * @param position where to add value
+	 * @return this Json object
+	 */
 	public Json add(String key, Object value, int position) {
 		List<Object> values = getList(key, Object.class);
 		if (values == null) {
@@ -405,51 +668,100 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return put(key, values);
 	}
 
+	/**
+	 * Get all entries in this object
+	 *
+	 * @return entries
+	 */
 	@Override
 	public Set<java.util.Map.Entry<String, Object>> entrySet() {
 		return datas.entrySet();
 	}
 
-	@Override
-	public void clear() {
-		datas.clear();
-	}
-
+	/**
+	 * Get all keys in this object
+	 *
+	 * @return keys
+	 */
 	@Override
 	public Set<String> keySet() {
 		return datas.keySet();
 	}
 
-	@Override
-	public Collection<Object> values() {
-		return datas.values();
-	}
 
+	/**
+	 * Get all keys in this object
+	 *
+	 * @return List of keys
+	 */
 	public List<String> keyList() {
 		List<String> keys = new ArrayList<>();
 		Collections.addAll(keys, datas.keySet().toArray(new String[0]));
 		return keys;
 	}
 
+	/**
+	 * Get all values in this object
+	 *
+	 * @return values
+	 */
+	@Override
+	public Collection<Object> values() {
+		return datas.values();
+	}
+
+	/**
+	 * Clear/Empty this Json object
+	 */
+	@Override
+	public void clear() {
+		datas.clear();
+	}
+
+	/**
+	 * Stringify this object as JavaScript standard
+	 *
+	 * @return a string usable in JavaScript
+	 */
 	@Override
 	public String toString() {
 		return XMLJsonParser.toJSON(this);
 	}
 
 
+	/**
+	 * Stringify this object as JavaScript standard in low weight/compressed
+	 *
+	 * @return a compressed string usable in JavaScript
+	 */
 	public String toString(boolean compressed) {
 		return XMLJsonParser.toJSON(this, compressed);
 	}
 
+	/**
+	 * To MongoDb document
+	 *
+	 * @return BsonDocument
+	 */
 	@Override
 	public <C> BsonDocument toBsonDocument(final Class<C> documentClass, final CodecRegistry codecRegistry) {
 		return new BsonDocumentWrapper<Json>(this, codecRegistry.get(Json.class));
 	}
 
+	/**
+	 * Get a hashed representation
+	 *
+	 * @return a hash of this object
+	 */
 	public String getHash() {
 		return "i" + Hidder.encodeString(String.valueOf(toString().hashCode()));
 	}
 
+	/**
+	 * Simple clone function
+	 *
+	 * @return a clone of this object
+	 */
 	@Override
 	public Json clone() {
 		try {
@@ -459,6 +771,11 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a List representation of this Object
+	 *
+	 * @return a List of the entries
+	 */
 	public List<Json> toList() {
 		List<Json> arr = new ArrayList<>();
 		for (Entry<String, Object> entry : datas.entrySet()) {
@@ -467,6 +784,11 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		return arr;
 	}
 
+	/**
+	 * Get the key of a value
+	 *
+	 * @return the first key where the value can be found
+	 */
 	public String findKey(Object value) {
 		if (value == null) {
 			return null;
@@ -480,6 +802,12 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 
 	}
 
+	/**
+	 * Get a DbTags
+	 *
+	 * @param key where find
+	 * @return a DbTags
+	 */
 	public DbTags getParent(String key) {
 		try {
 			return new DbTags(getString(key));
@@ -488,6 +816,12 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Get a List of DbTags
+	 *
+	 * @param key where find
+	 * @return a List of DbTags
+	 */
 	public List<DbTags> getParents(String key) {
 		try {
 			List<DbTags> parents = new ArrayList<>();
@@ -500,6 +834,11 @@ public class Json implements Map<String, Object>, Serializable, Bson {
 		}
 	}
 
+	/**
+	 * Sort entries by key
+	 *
+	 * @return Json where entries are alphabetical ordered
+	 */
 	public Json sort() {
 		List<String> keys = Arrays.asList(keySet().toArray(new String[0]));
 		Collections.sort(keys);
