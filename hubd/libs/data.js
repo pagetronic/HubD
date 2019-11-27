03b74a259f437cfaa89c16e485c994de5c2adc98
pagetronic <pagetronic@gmail.com>
2019-11-27 18:16:26 +0400

    comment and clean

--- hubd/libs/data.js
+++ hubd/libs/data.js
@@ -1,30 +1,48 @@
 var data = {
-	set: function (key, data, temp) {
-		var store = (temp !== undefined && temp) ? window.sessionStorage : window.localStorage;
-		if (store === undefined) {
-			return;
-		}
-		key = "data_" + key;
-		store.setItem(key, JSON.stringify(data));
-	},
-	get: function (key, temp, def) {
-		var store = (temp !== undefined && temp) ? window.sessionStorage : window.localStorage;
-		if ( store ===  undefined) {
-			return (def !== undefined) ? def : null;
-		}
-		key = "data_" + key;
-		if (store.getItem(key) !== null) {
-			return JSON.parse(store.getItem(key));
-		} else {
-			return (def !== undefined) ? def : null;
-		}
-	},
-	remove: function (key, temp) {
-		var store = (temp !== undefined && temp) ? window.sessionStorage : window.localStorage;
-		if ( store ===  undefined) {
-			return null;
-		}
-		key = "data_" + key;
-		store.removeItem(key)
-	}
-}
+    /**
+     * Save data at key
+     *
+     * @param key where store data
+     * @param data who stored at key
+     * @param temp is for session ?
+     */
+    set: function (key, data, temp) {
+        var store = temp === true ? window.sessionStorage : window.localStorage;
+        if (store === undefined) {
+            return;
+        }
+        store.setItem("data_" + key, JSON.stringify(data));
+    },
+    /**
+     * Get data at key
+     *
+     * @param key where store data
+     * @param temp is for session ?
+     * @param def failure value
+     */
+    get: function (key, temp, def) {
+        var store = temp === true ? window.sessionStorage : window.localStorage;
+        if (store === undefined) {
+            return (def !== undefined) ? def : null;
+        }
+        key = "data_" + key;
+        if (store.getItem(key) !== null) {
+            return JSON.parse(store.getItem(key));
+        } else {
+            return (def !== undefined) ? def : null;
+        }
+    },
+    /**
+     * Remove data at key
+     *
+     * @param key where to remove
+     * @param temp is for session ?
+     */
+    remove: function (key, temp) {
+        var store = temp === true ? window.sessionStorage : window.localStorage;
+        if (store === undefined) {
+            return null;
+        }
+        store.removeItem("data_" + key)
+    }
+};
