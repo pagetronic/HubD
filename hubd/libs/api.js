2697ee8367bd0c6a9c8ee7c531dba232fbc7cc0b
pagetronic <pagetronic@gmail.com>
2019-11-27 17:59:20 +0400

    comment and clean

--- hubd/libs/api.js
+++ hubd/libs/api.js
@@ -1,135 +1,103 @@
 var api = {
-	post: function (url, data, successFunc, errorFunc, api) {
-		if ( api ===  undefined) {
-			api = true;
-		}
-		if (data !== undefined && data.lng === undefined) {
-			data.lng = sys.lng
-		}
-		var xhrapi = $.ajax({
-			type: "POST",
-			url: (api ? constants.apiurl : '') + url,
-			dataType: "json",
-			xhrFields: {
-				withCredentials: true
-			},
-			headers: {"Content-Type": "application/json", "X-Requested-With": "XMLHttpRequest"},
-			data: JSON.stringify(data),
-			success: function (json) {
-				if ( successFunc !==  undefined) {
-					try {
-						successFunc(json);
-					} catch (e) {
-						log(e);
-					}
-				}
-			},
-			error: function (e) {
-				if (e.statusText !== 'abort') {
-					if ( errorFunc !==  undefined) {
-						try {
-							errorFunc(e.responseJSON);
-						} catch (e) {
-							log(e);
-						}
-					}
-					sys.wait(false);
-				}
-			}
-		});
-		return xhrapi;
-	},
-	get: function (url, data, successFunc, errorFunc, api) {
+    /**
+     * Post data to Api
+     * @param url path of the request
+     * @param data object to send to api
+     * @param successFunc function to execute on success
+     * @param errorFunc function to execute on error
+     * @param api set to false if request is on the current domain
+     * @returns XMLHttpRequest object
+     */
+    post: function (url, data, successFunc, errorFunc, api) {
+        if (api === undefined) {
+            api = true;
+        }
+        if (data !== undefined && data.lng === undefined) {
+            data.lng = sys.lng
+        }
+        return $.ajax({
+            type: "POST",
+            url: (api ? constants.apiurl : '') + url,
+            dataType: "json",
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {"Content-Type": "application/json", "X-Requested-With": "XMLHttpRequest"},
+            data: JSON.stringify(data),
+            success: function (json) {
+                if (successFunc !== undefined) {
+                    try {
+                        successFunc(json);
+                    } catch (e) {
+                        log(e);
+                    }
+                }
+            },
+            error: function (e) {
+                if (e.statusText !== 'abort') {
+                    if (errorFunc !== undefined) {
+                        try {
+                            errorFunc(e.responseJSON);
+                        } catch (e) {
+                            log(e);
+                        }
+                    }
+                    sys.wait(false);
+                }
+            }
+        });
+    },
+    /**
+     * Get data from Api
+     * @param url path of the request
+     * @param data object to send to api (query parameters)
+     * @param successFunc function to execute on success
+     * @param errorFunc function to execute on error
+     * @param api set to false if request is on the current domain
+     * @returns XMLHttpRequest object
+     */
+    get: function (url, data, successFunc, errorFunc, api) {
 
-		if (typeof data === "function") {
-			successFunc = data;
-			errorFunc = successFunc;
-			api = errorFunc;
-		}
-		if ( api ===  undefined) {
-			api = true;
-		}
-		var xhrapi = $.ajax({
-			type: "GET",
-			url: (api ? constants.apiurl : '') + url,
-			dataType: "json",
-			xhrFields: {
-				withCredentials: true
-			},
-			headers: {"Content-Type": "application/json", "X-Requested-With": "XMLHttpRequest"},
-			data: data,
-			success: function (json) {
-				if ( successFunc !==  undefined) {
-					try {
-						successFunc(json);
-					} catch (e) {
-						log(e);
-					}
-				}
+        if (typeof data === "function") {
+            successFunc = data;
+            errorFunc = successFunc;
+            api = errorFunc;
+        }
+        if (api === undefined) {
+            api = true;
+        }
+        var xhrapi = $.ajax({
+            type: "GET",
+            url: (api ? constants.apiurl : '') + url,
+            dataType: "json",
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {"Content-Type": "application/json", "X-Requested-With": "XMLHttpRequest"},
+            data: data,
+            success: function (json) {
+                if (successFunc !== undefined) {
+                    try {
+                        successFunc(json);
+                    } catch (e) {
+                        log(e);
+                    }
+                }
 
-			},
-			error: function (e) {
-				if (e.statusText !== 'abort') {
-					if ( errorFunc !==  undefined) {
-						try {
-							errorFunc(e.responseJSON);
-						} catch (e) {
-							log(e);
-						}
-					}
-					sys.wait(false);
-				}
-			}
-		});
-		return xhrapi;
-	},
-	paginer: function (url, ul, liCreator, data, key) {
-
-		var scrollerFinder = function () {
-			return ul.isScrollable() ? ul : ul.scrollParent();
-		};
-
-		var scroller = scrollerFinder();
-		var paging;
-		var loading = sys.loading(30, 'li');
-		var get = function () {
-			scroller.off('scroll.paginer');
-			ul.append(loading);
-			api.get(url, {paging: paging}, make);
-		};
-		var make = function (data) {
-			if (key !== undefined && data[key] !== undefined) {
-				data = data[key];
-			}
-			loading.detach();
-			if (data.result.length === 0 && paging === undefined) {
-				ul.html($('<li/>').html(lang.get('EMPTY')));
-				return;
-			}
-
-			var li;
-			$.each(data.result, function (i, item) {
-				li = liCreator(item);
-				ul.append(li);
-				ajax.it(li);
-			});
-
-			paging = data.paging.next;
-			if (paging !== undefined) {
-				scroller = scrollerFinder();
-				scroller.on('scroll.paginer', function (e) {
-					if (li.isInView()) {
-						get();
-					}
-				}).trigger('scroll');
-			}
-
-		};
-
-		if (data !== undefined) {
-			make(data);
-		} else {
-			get();
-		}
-	}
+            },
+            error: function (e) {
+                if (e.statusText !== 'abort') {
+                    if (errorFunc !== undefined) {
+                        try {
+                            errorFunc(e.responseJSON);
+                        } catch (e) {
+                            log(e);
+                        }
+                    }
+                    sys.wait(false);
+                }
+            }
+        });
+        return xhrapi;
+    }
 };
