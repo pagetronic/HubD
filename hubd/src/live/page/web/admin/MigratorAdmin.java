bc7e67f02d9c0e053f8181d05009b2094773ce70
pagetronic <pagetronic@gmail.com>
2020-04-22 14:40:20 +0400

    Admin function for pages transfer to another website

--- hubd/src/live/page/web/admin/MigratorAdmin.java
+++ hubd/src/live/page/web/admin/MigratorAdmin.java
@@ -6,9 +6,7 @@ package live.page.web.admin;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.*;
 import live.page.web.system.Settings;
 import live.page.web.system.db.Db;
 import live.page.web.system.json.Json;
@@ -18,6 +16,7 @@ import live.page.web.system.servlet.wrapper.ApiServletResponse;
 import live.page.web.system.servlet.wrapper.WebServletRequest;
 import live.page.web.system.servlet.wrapper.WebServletResponse;
 import live.page.web.system.sessions.Users;
+import org.bson.BsonUndefined;
 import org.bson.conversions.Bson;
 
 import javax.servlet.ServletException;
@@ -25,7 +24,9 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @WebServlet(urlPatterns = {"/admin/migrator"})
 public class MigratorAdmin extends HttpServlet {
@@ -54,7 +55,7 @@ public class MigratorAdmin extends HttpServlet {
 				rez = searchDestination(data.getString("search"));
 				break;
 			case "migrate":
-				rez = migrate(data.getId(), data.getString("destination"));
+				rez = migrate(user, data.getId(), data.getString("destination"));
 				break;
 		}
 		resp.sendResponse(rez);
@@ -66,11 +67,9 @@ public class MigratorAdmin extends HttpServlet {
 		MongoCollection<Json> pages_collection = destination_base.getCollection("Pages", Json.class);
 		int limit = 40;
 		List<Bson> pipeline = new ArrayList<>();
-		List<Bson> filters = new ArrayList<>();
 
 		if (search != null && !search.equals("")) {
-			filters.add(Filters.text(search));
-			pipeline.add(Aggregates.match(Filters.and(filters)));
+			pipeline.add(Aggregates.match(Filters.or(Filters.regex("title", Pattern.compile(search, Pattern.CASE_INSENSITIVE)), Filters.text(search))));
 			pipeline.add(Aggregates.sort(Sorts.orderBy(Sorts.metaTextScore("score"), Sorts.ascending("_id"))));
 		} else {
 			pipeline.add(Aggregates.sort(Sorts.orderBy(Sorts.descending("update"), Sorts.ascending("_id"))));
@@ -84,15 +83,76 @@ public class MigratorAdmin extends HttpServlet {
 		return new Json("result", pages);
 	}
 
-	// TODO migrate all childs
-	private Json migrate(String id, String destination) {
+	private Json migrate(Users user, String id, String destination) {
 		if (id == null || destination == null || id.equals("") || destination.equals("")) {
 			return new Json("ok", false);
 		}
 		MongoClient destination_client = Db.getClient(Settings.MIGRATOR_DB_USER, Settings.MIGRATOR_DB_NAME, Settings.MIGRATOR_DB_PASS);
 		MongoDatabase destination_base = destination_client.getDatabase(Settings.MIGRATOR_DB_NAME);
+		MongoCollection<Json> destination_pages = destination_base.getCollection("Pages", Json.class);
+		List<Json> pages = Db.aggregate("Pages",
+				Arrays.asList(
+						Aggregates.match(Filters.eq("_id", id)),
+						Aggregates.limit(1),
+						Aggregates.graphLookup("Pages", "$_id", "_id", "parents.0", "childs", new GraphLookupOptions().depthField("depth").maxDepth(5000)),
+						Aggregates.lookup("Pages", "_id", "_id", "page"),
+						Aggregates.project(new Json("branche", new Json("$concatArrays", Arrays.asList("$page", "$childs")))),
+						Aggregates.unwind("$branche"),
+						Aggregates.replaceRoot("$branche"),
+						Aggregates.addFields(
+								new Field<>("depth", new Json("$cond",
+										Arrays.asList(
+												new Json("$eq", Arrays.asList("$depth", new BsonUndefined())),
+												-1, "$depth")
+								))
+						),
+						Aggregates.sort(new Json("depth", 1))
+				)).into(new ArrayList<>());
 
-		return null;
+		Json rez = new Json("ok", true);
+		for (Json page : pages) {
+			if (destination_pages.countDocuments(Filters.eq("_id", page.getId()), new CountOptions().limit(1)) > 0) {
+				return new Json("ok", false).put("error", "DUPLICATE_ID");
+			}
+		}
+		for (Json page : pages) {
+			Json original_page = page.clone();
+			if (page.getId().equals(id)) {
+				page.add("parents", destination, 0);
+			}
+			while (destination_pages.countDocuments(Filters.eq("url", page.getString("url")), new CountOptions().limit(1)) > 0) {
+				page.put("url", page.getString("url") + "~");
+			}
+
+			destination_pages.insertOne(page);
+
+
+			List<Bson> pipeline = new ArrayList<>();
+
+			pipeline.add(Aggregates.match(Filters.eq("_id", page.getId())));
+			pipeline.add(Aggregates.limit(1));
+			pipeline.add(Aggregates.graphLookup("Pages", "$_id", "parents.0", "_id", "urls", new GraphLookupOptions().depthField("depth").maxDepth(50)));
+			pipeline.add(Aggregates.unwind("$urls", new UnwindOptions().preserveNullAndEmptyArrays(true)));
+			pipeline.add(Aggregates.sort(new Json("urls.depth", -1)));
+
+			pipeline.add(Aggregates.group("$_id",
+					Arrays.asList(
+							Accumulators.first("lng", "$lng"),
+							Accumulators.push("urls", "$urls.url")
+					)));
+
+			pipeline.add(Aggregates.project(new Json()
+					.put("url", new Json("$reduce", new Json("input", "$urls").put("initialValue", Settings.HTTP_PROTO + Settings.MIGRATOR_LANGS_DOMAINS.getString(page.getString("lng"))).put("in", new Json("$concat", Arrays.asList("$$value", "/", "$$this"))))))
+			);
+			String url = destination_pages.aggregate(pipeline).first().getString("url");
+
+			Db.save("Revisions", new Json("origine", page.getId())
+					.put("editor", user.getId()).put("url", original_page.getString("url")).put("301", url).put("edit", new Date()));
+			Db.deleteOne("Pages", Filters.eq("_id", original_page.getId()));
+			rez.add("redirects", new Json("origin", page.getString("url")).put("destination", url));
+		}
+		destination_client.close();
+		return rez;
 	}
 
 
