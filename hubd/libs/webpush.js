a3781db4ce50d2cdc0af9e5499a34fbfa86f13ca
pagetronic <pagetronic@gmail.com>
2021-01-10 16:01:54 +0100

    debug

--- hubd/libs/webpush.js
+++ hubd/libs/webpush.js
@@ -59,7 +59,7 @@ var webpush = {
         webpush.worker(function () {
             Notification.requestPermission().then(function (result) {
                 if (result !== 'granted') {
-                    sys.confirm(lang.get('SUBSCRIPTION_LOCKED'), 10, function () {
+                    sys.confirm(lang.get('CONFIRMATION', 'ucfirst'), lang.get('SUBSCRIPTION_LOCKED'), 10, function () {
                         webpush.enable(obj, onsuccess, onerror);
                     }, function () {
                         onerror();
