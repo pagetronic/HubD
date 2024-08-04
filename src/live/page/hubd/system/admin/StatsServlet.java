a0842f475fc8410b3cd89dd96b4ce4aa4d8ea025
pagetronic <pagetronic@gmail.com>
2024-08-04 15:06:17 +0200

    #stats

---
+++ src/live/page/hubd/system/admin/StatsServlet.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Laurent PAGE, Apache Licence 2.0
+ */
+package live.page.hubd.system.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import live.page.hubd.system.StatsTools;
+import live.page.hubd.system.json.Json;
+import live.page.hubd.system.servlet.HttpServlet;
+import live.page.hubd.system.servlet.wrapper.ApiServletRequest;
+import live.page.hubd.system.servlet.wrapper.ApiServletResponse;
+import live.page.hubd.system.sessions.Users;
+
+import java.io.IOException;
+
+@WebServlet(asyncSupported = true, urlPatterns = {"/admin/stats"})
+public class StatsServlet extends HttpServlet {
+
+
+    @Override
+    public void doGetApi(ApiServletRequest req, ApiServletResponse resp, Users user) throws IOException, ServletException {
+
+        if (!user.getAdmin()) {
+            resp.sendResponse(new Json("error", "UNKNOWN_METHOD"));
+        }
+        resp.sendResponse(new Json()
+                .put("stats", StatsTools.getSimplesStats(req.getTz()))
+                .put("urls", StatsTools.getStatsUrl(req.getTz())));
+
+
+    }
+
+}
